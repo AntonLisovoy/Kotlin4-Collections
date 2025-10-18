a9722c484b44f35925c094b66f95531a274445a8
Антон Лисовой <alisovoy@moysklad.ru>
2025-10-18 12:19:17 +0300

    Сортировка справочника по имени

--- src/main/kotlin/ru/otus/homework/persons/persons.kt
+++ src/main/kotlin/ru/otus/homework/persons/persons.kt
@@ -10,4 +10,4 @@ fun List<Person>.sortByAge(): List<Person> = sortedByDescending { it.age }
  * - Фамилии сортируются по алфавиту в порядке возрастания
  * - Если фамилии совпадают, персоны сортируются по имени в порядке возрастания
  */
-fun List<Person>.sortByName(): List<Person> = TODO("Доделать sortBySurname")
+fun List<Person>.sortByName(): List<Person> = sortedWith(compareBy({ it.surname }, { it.name }))
